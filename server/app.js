ae9f3fb8179fb1837a1fa6b679344624b8eca980
anukalprauniyar <anukalpkumar2000@gmail.com>
2023-07-20 19:42:57 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const jwt = require('jsonwebtoken');
+
 app.use(cors())
 
 app.use(express.json());
@@ -23,21 +25,25 @@ const dummyUser = [
     }
     ];
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
 
     const user = dummyUser.find(u => u.email === email && u.password === password)
   
     if (user) {
-      // Authentication successful
-      res.status(200).json({ message: 'Login successful' });
+      
+      const secretKey = 'hexadecimal2023'; // Replace with your own secret key
+      const token = jwt.sign({ userId: user.email}, secretKey, { expiresIn: '1h' });
+      res.json({token});
+
     } else {
       // Authentication failed
       res.status(401).json({ message: 'Invalid username or password' });
     }
   });
 
+  
 app.listen(5000, ()=>{
     console.log(`Server is running on port number 5000`);
 })
